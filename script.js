f692d743ef1a8d222274d7fef96fbea9193904f5
Prasad750 <138876806+Prasad750@users.noreply.github.com>
2023-11-17 23:34:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const firstName=document.getElementById("fn");
+const lastName=document.getElementById("ln");
+const phoneNumber=document.getElementById("pn");
+const emailId=document.getElementById("email");
+function myAlert()
+	{
+		alert(`First Name: ${firstName.value} \nLast Name: ${lastName.value} \nPhone Number: ${phoneNumber.value} \nEmail Id: ${emailId.value}`);
+	}
